749120537eff9df2e37de5427fbebaf60d7a8bc1
Zack <sligerzack@gmail.com>
2019-03-13 15:12:44 -0400

    Caesar shift progress and README update.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Ciphers
 Uses react and react bootstrap
 
-# React Bootstrap Installation
+## Uses
+- React
+
+## React Bootstrap Installation
 npm install react-bootstrap bootstrap
+
+## Design In-Progress
+
+### Caesar Shift Functions
+~~- Shift to specifications~~
+- Get all shifts
+~~- Retrieve user input~~
+- Display shift or shifts.
+
+##### Others
+- Limit input for how to shift
+- Give options to encrypt or decrypt. (Could just display all shifts, will likely do this.)
+- Display shifts as text is given.
+
+### Monoalphabetic Functions
+-
+-
+-
+-
--- ciphers/src/Components/Caesar.js
+++ ciphers/src/Components/Caesar.js
@@ -1,20 +1,83 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import Form from 'react-bootstrap/Form'
 
 class Caesar extends Component{
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.caesar_shift = this.caesar_shift.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    ReactDOM.render(
+      <Col>{this.caesar_shift(event.target.value, 1)}</Col>,
+      document.getElementById('caesar-output')
+    );
+  }
+
+  caesar_shift(str, shift_num){
+    str = str.toLowerCase(); // Probably should also check for only characters in alphabet.
+    var alpha = "abcdefghijklmnopqrstuvwxyz";
+    var shift = "";
+    for(var i=0; i<str.length; i++){
+      var pos = alpha.indexOf(str.charAt(i)) + shift_num;
+      if (pos > 25) pos = pos - 26;
+      shift = shift.concat(alpha.charAt(pos));
+    }
+    console.log(shift);
+    return shift;
+  }
+
   render() {
     return (
       <div className="Caesar">
-        <div class="container">
-          Hello Caesar Here!
-          <Form.Group controlId="caesar-input">
-            <Form.Label>Insert Text</Form.Label>
-            <Form.Control as="textarea" rows="3" />
-          </Form.Group>
-        </div>
+        <Container>
+          <Row>
+            <Col>
+              <Form.Group controlId="caesar-input">
+                <Form.Label>Insert Text</Form.Label>
+                <Form.Control as="textarea" rows="3" onChange={this.handleChange}/>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row id="caesar-output"/>
+        </Container>
       </div>
       );
   }
 }
+
+/*
+function get_shifts(input){
+  var shifts = [];
+  var i;
+  for(i=1; i<26; i++){
+    shifts.push(caesar_shift(input, i));
+  }
+  return shifts;
+}
+*/
+/*
+function caesar_shift(str, shift_num){
+  str = str.toLowerCase(); // Probably should also check for only characters in alphabet.
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var shift = "";
+  for(var i=0; i<str.length; i++){
+    var character = alpha.charAt(alpha.indexOf(str.charAt(i)) + shift_num);
+    shift.concat(character);
+    console.log("PASS");
+    console.log(i);
+    console.log(character);
+    console.log(shift);
+  }
+  return shift;
+}
+*/
 
 export default Caesar;
